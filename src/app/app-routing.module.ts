bdd586f4d557aa6f4b700b4b3b55af161b48c495
dudushy <eduardotalarico1337@gmail.com>
2022-07-09 22:38:22 -0300

    Create draw page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
+  // {
+  //   path: 'draw',
+  //   loadChildren: () => import('./draw/draw.module').then( m => m.DrawPageModule)
+  // }
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./draw/draw.module').then( m => m.DrawPageModule)
   }
 ];
 @NgModule({
